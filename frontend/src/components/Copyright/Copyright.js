86294151ae96ee64c197f85cf4af5740fa54eb89
Varun Sam <varunsam1094@gmail.com>
2018-09-11 01:28:27 +0800

    First Commit

---
+++ frontend/src/components/Copyright/Copyright.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {  Container, Footer } from 'mdbreact';
+
+class FooterPage extends React.Component {
+    render(){
+        return(
+            <Footer color="black" className="font-small pt-4 mt-4">
+                <div className="footer-copyright text-center py-3">
+                    <Container fluid>
+                        &copy; {(new Date().getFullYear())} Copyright: <a href="https://www.onwardsmediagroup.com/"> OnwardsMediaGroup </a>
+                    </Container>
+                </div>
+            </Footer>
+        );
+    }
+}
+
+export default FooterPage;
---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import compLogo from '../../assets/Logo.png';
+const logo = (props) => (
+    <div className="text-center img-fluid">
+        <img src={compLogo} alt="MyLogo"/>
+    </div>
+);
+
+export default logo;
